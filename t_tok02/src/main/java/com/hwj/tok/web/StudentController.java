530eaa1e4ba649f3d6467be2b4b94a3ce7ebc05f
jaelik <whowhoman@naver.com>
2023-02-09 20:33:09 +0900

    mvc 통해 여러가 구현 방법 1.ModelAndView 2.@ModelAttribute

---
+++ t_tok02/src/main/java/com/hwj/tok/web/StudentController.java
@@ -0,0 +1,40 @@
+package com.hwj.tok.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.hwj.tok.model.Student;
+
+@Controller
+public class StudentController {
+	private static final Logger logger = LoggerFactory.getLogger(StudentController.class);
+	
+	//방법 1. return 값을 String형으로 만들어주고, Model을 따로 장착 시키는 방법 
+	// - return은 view의 이름을 명시해주고 , view에서 사용될 model는 따로 주입을 하는 방법 
+	@RequestMapping(value = "/student01.do",method = RequestMethod.GET)
+	public String student01(Model model) {
+		model.addAttribute("student",new Student());
+		return "student/student";
+	}
+	
+	//방법2. ModelAndView를 활용 하는방식 
+	//- 방법으1의 단계를 한개로 합친 것 
+	
+	@RequestMapping(value ="/student02.do",method = RequestMethod.GET )
+	public ModelAndView student02() {
+		return new ModelAndView("student/student","student",new Student());
+	}
+	
+	//student.jsp의 요청에서 입력 값을 받아서 result.jsp로 전달하는 맵핑 
+	@RequestMapping(value = "/add/student.do", method=RequestMethod.POST)
+	public String addStudent(@ModelAttribute Student student, Model model) {
+		model.addAttribute("student",student);
+		return "student/result";
+	}
+}
