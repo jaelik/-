39a345d1f577ffa183e87db4538a5c18e929de22
jaelik <whowhoman@naver.com>
2023-02-10 23:35:57 +0900

    DB 연결 설정 및 dao로 DI의존성 주입으로 sql inset문 구성

--- t_tok02/src/main/java/com/hwj/tok/web/StudentController.java
+++ t_tok02/src/main/java/com/hwj/tok/web/StudentController.java
@@ -2,6 +2,7 @@ package com.hwj.tok.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -9,12 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.hwj.tok.dao.StudentDAO;
 import com.hwj.tok.model.Student;
 
 @Controller
 public class StudentController {
 	private static final Logger logger = LoggerFactory.getLogger(StudentController.class);
 	
+	@Autowired
+	private StudentDAO studentDAO;
 	//방법 1. return 값을 String형으로 만들어주고, Model을 따로 장착 시키는 방법 
 	// - return은 view의 이름을 명시해주고 , view에서 사용될 model는 따로 주입을 하는 방법 
 	@RequestMapping(value = "/student01.do",method = RequestMethod.GET)
@@ -35,6 +39,9 @@ public class StudentController {
 	@RequestMapping(value = "/add/student.do", method=RequestMethod.POST)
 	public String addStudent(@ModelAttribute Student student, Model model) {
 		model.addAttribute("student",student);
+		//학생 객체를 DB에 입력 
+		studentDAO.create(student);
 		return "student/result";
 	}
+	
 }
