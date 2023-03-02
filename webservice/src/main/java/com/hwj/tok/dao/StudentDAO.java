1d17fa8e9f7ea07f153053f2c23fdd11dc83df2e
jaelik <whowhoman@naver.com>
2023-03-02 18:39:10 +0900

    웹 서비스 -네트워크 상에서 서로 다른 종류의 컴퓨터들 간에 상호 작용을 하기 위한 소프트웨어 시스템 -웹을 통해 서비스를 제공하는것 -ex) 학생 관리 서비스 (CRUD) Create 등록 Read 조회 Upadate 수정 Delete 삭제

---
+++ webservice/src/main/java/com/hwj/tok/dao/StudentDAO.java
@@ -0,0 +1,66 @@
+package com.hwj.tok.dao;
+
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.hwj.tok.mapper.StudentMapper;
+import com.hwj.tok.model.Student;
+//dao 사용시 사용되는 어노테이션  Component ,Repository
+@Component
+public class StudentDAO {
+	private JdbcTemplate jdbcTemplate;
+	
+	@Autowired
+	public void SetDataSource(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+	
+	//학생 등록 메서드 
+	public void create(Student student) {
+		String sql = "INSERT INTO Student(id, name, age) values(?,?,?)";
+		jdbcTemplate.update(sql,student.getId(),student.getName(),student.getAge());
+	}
+	
+	
+	//학생 조회 메서드 -리스트 
+	public List<Student>  select( ) {
+		String sql = "SELECT * FROM Student";
+		List<Student> students = jdbcTemplate.query(sql, new StudentMapper());
+		System.out.println("student>>dao>>"+students);
+		return students;
+	}
+	//학생 조회 메서드 -단건  
+	public Student select(Integer seq ) {
+		String sql = "SELECT * FROM Student WHERE seq = ?";
+		Student students = jdbcTemplate.queryForObject(sql,new Object[] {seq},new StudentMapper());
+		return students;
+	}
+	
+	//학생 수정 메서드 
+	
+	public void update (Student student) {
+		String sql = "UPDATE Student SET id=?,name=?,age=? WHERE seq=?";
+		jdbcTemplate.update(sql,student.getId(),student.getName(),student.getAge(),student.getSeq());
+	}
+	
+	//학생 삭제 메서드 
+	public Integer delete (Integer seq) {
+		String sql = "DELETE FROM Student WHERE seq = ?";
+		return jdbcTemplate.update(sql,seq);
+	}
+	//학생 입력후 에러가 발생하는 학생 수정을 호출 하는 상황 
+//	@Transactional
+//	//update 문에  없는 테이블이 있는 상황이였고 어노테이션 @Transactional을 걸게되면  하나의 작업단위로 인지
+//	// update 문이 실행이 안되었을시 모든 작업을 실행을 안하게되어 create 문도 실행이 안되 db에 저장이 안되게된다.
+//	public void sampleTransaction(Student student) {
+//		this.create(student);
+//		this.update(student);
+//	}
+}
